0b1368d6af52e72a7c557de3ee6c2c202f7b8685
syuichi4989 <167294339+syuichi4989@users.noreply.github.com>
2024-05-14 16:25:25 +0900

    Add files via upload

--- js/vegas.js
+++ js/vegas.js
@@ -1,8 +1,8 @@
 $(function() {
     $('.mainimg-tate').vegas({
         slides: [
-            { src: 'images/IMG_0128.jpeg' },
-            { src: 'images/IMG_0142.jpeg' },	//1枚目の写真指定
+            { src: 'images/IMG_0128.jpg' },
+            { src: 'images/IMG_0142.jpg' },	//1枚目の写真指定
             { src: 'kaito.jpeg' },	//2枚目の写真指定
            	
         ],
